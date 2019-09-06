79c5e4dbe5041d966e5f4085365f75727cdf98f6
Jorge Hernandez <ing.jorgeh@gmail.com>
2019-09-06 18:09:42 -0500

    Se implementa actuator

--- src/main/java/com/fromacionbdi/springboot/app/item/controllers/ItemController.java
+++ src/main/java/com/fromacionbdi/springboot/app/item/controllers/ItemController.java
@@ -1,9 +1,18 @@
 package com.fromacionbdi.springboot.app.item.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,26 +22,35 @@ import com.fromacionbdi.springboot.app.item.models.Producto;
 import com.fromacionbdi.springboot.app.item.services.ItemService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
+@RefreshScope
 @RestController
 public class ItemController {
+
+	private Logger log = LoggerFactory.getLogger(ItemController.class);
+	
+	@Autowired
+	private Environment env;
 
 	@Autowired
 	@Qualifier("restTemplateService")
 	private ItemService itemService;
-	
+
+	@Value("${configuracion.texto}")
+	private String text;
+
 	@GetMapping("/listar")
-	public List<Item> listar(){
+	public List<Item> listar() {
 		return itemService.findAll();
 	}
-	
-	//@HystrixCommand(fallbackMethod = "metodoAlternativo")
+
+	@HystrixCommand(fallbackMethod = "metodoAlternativo")
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
-	public Item detalle (@PathVariable Long id, @PathVariable Integer cantidad){
+	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return itemService.findById(id, cantidad);
 	}
-	
-	public Item metodoAlternativo(Long id,Integer cantidad) {
-		Item item= new Item();
+
+	public Item metodoAlternativo(Long id, Integer cantidad) {
+		Item item = new Item();
 		Producto producto = new Producto();
 		item.setCantidad(cantidad);
 		producto.setId(id);
@@ -41,4 +59,18 @@ public class ItemController {
 		item.setProducto(producto);
 		return item;
 	}
+
+	@GetMapping("/optener-config")
+	public ResponseEntity<?> obetenerConfiguracion(@Value("${server.port}") String puerto) {
+		log.info(text);
+		Map<String, String> json = new HashMap<>();
+		json.put("texto", text);
+		json.put("puerto", puerto);
+		if(env.getActiveProfiles().length>0 && env.getActiveProfiles()[0].equals("dev")) {
+			json.put("nombre", env.getProperty("configuracion.autor.nombre"));
+			json.put("email", env.getProperty("configuracion.autor.email"));
+		}
+		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
+	}
+
 }
